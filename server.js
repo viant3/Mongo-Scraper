93f80b161aecac0e2179b13c1a50f6071a8bdb81
victor politano <viant3ucf@gmail.com>
2019-10-16 18:18:01 -0400

    changed route from scrape to /

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(MONGODB_URI);
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/", function(req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://www.nytimes.com/section/world").then((response) => {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
