5e80fa00a0d4613f00911af7de77d0ec0a95a6d0
victor politano <viant3ucf@gmail.com>
2019-10-19 06:58:29 -0400

    changed port to equal process.env.port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3002
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 
-var MONGODB_URI = process.env.PORT || "mongodb://localhost/nytScraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytScraper";
 
 mongoose.connect(MONGODB_URI);
 // mongoose.connect("mongodb://localhost/nytScraper", { useNewUrlParser: true });
